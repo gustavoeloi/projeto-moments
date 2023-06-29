e8bf9f511b38f8b8a550c378b52951127693c11e
gustavooeloi <gustavooeloi@gmail.com>
2023-06-29 20:42:50 -0300

    adicionando os arquivos ao github

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Moment } from 'src/app/Moments';
+import { MomentService } from 'src/app/services/moment.service';
+import { Router } from '@angular/router';
+
+import { MessagesService } from 'src/app/services/messages.service';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css'],
+})
+export class NewMomentComponent {
+  btnText = 'Compartilhar!';
+
+  constructor(
+    private momentService: MomentService,
+    private messageService: MessagesService,
+    private router: Router
+  ) {}
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    // To-do
+
+    await this.momentService.createMoment(formData).subscribe();
+
+    this.messageService.add('Momento adicionado com sucesso!');
+
+    this.router.navigate(['/']);
+  }
+}
